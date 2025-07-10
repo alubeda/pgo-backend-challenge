b8b46e2f05ff5be84d5127b74ac9f139fd5abe26
alubeda <56682481+alubeda@users.noreply.github.com>
2025-07-10 16:47:57 +0200

    Add initial implementation of task endpoints: GET, POST, DELETE

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get, Post, Param, Body, Query } from '@nestjs/common';
-import { TasksService } from './tasks.service';
+import { Controller, Get, Post, Delete, Param, Body, Query, ParseIntPipe } from '@nestjs/common';
+import { TasksService, Task } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
-  // TODO: Implement GET /tasks
-  // TODO: Implement GET /tasks/:id
-  // TODO: Implement POST /tasks
-  // TODO: Implement DELETE /tasks/:id
+  // GET /tasks
+  @Get()
+  findAll(): Task[] {
+    return this.tasksService.findAll();
+  }
+
+  // GET /tasks/:id
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.findOne(id);
+  }
+
+  // POST /tasks
+  @Post()
+  create(@Body() createTaskDto: any) {
+    return this.tasksService.create(createTaskDto);
+  }
+
+  // DELETE /tasks/:id
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.remove(id);
+  }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,47 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  isDone: boolean;
+}
+
 @Injectable()
 export class TasksService {
-  // TODO: Add in-memory storage
-  // TODO: Implement findAll(), findOne(), create(), delete()
+  // emulate a database incrementing id sequence
+  private idSequence: number = 2;
+
+  private tasks: Task[] = [
+    { id: 1, title: 'Task 1', description: 'This is a task', isDone: false },
+    { id: 2, title: 'Task 2', description: 'This is another task', isDone: true },
+  ];
+
+  findAll(): Task[] {
+    return this.tasks;
+  }
+
+  findOne(id: number): Task | null {
+    return this.tasks.find((task) => task.id === id) || null;
+  }
+
+  // TODO: Use an actual DTO
+  create(createTaskDto: any): Task {
+    const task: Task = {
+      id: ++this.idSequence,
+      title: createTaskDto.title,
+      description: createTaskDto.description,
+      isDone: false,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  remove(id: number): Task | null {
+    const taskIndex: number = this.tasks.findIndex((task) => task.id === id);
+
+    return taskIndex === -1 ? null : this.tasks.splice(taskIndex, 1)[0];
+  }
 }
